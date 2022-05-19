ddfdeca4d0b78d5ac99bc509839cee31a76a3076
nicolagardo <nicolagardo@gmail.com>
2022-05-18 21:37:45 -0300

    components created

--- frontend/mi-porfolio/src/app/app.module.ts
+++ frontend/mi-porfolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    ToolbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
